b0b91791ae567178dc5330a3f31f8364f818e69a
nobuhitotateyama@gmail.com <nobuhitotateyama@gmail.com>
2023-01-08 01:38:30 +0900

    add vuetify.

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import store from './store';
+import vuetify from './plugins/vuetify';
 import { loadFonts } from './plugins/webfontloader';
 
 loadFonts();
 
-createApp(App).use(store).mount('#app');
+createApp(App).use(store).use(vuetify).mount('#app');
